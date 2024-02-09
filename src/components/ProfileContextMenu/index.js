36bc4d11dcb2b23a7470a26c45452989240fdd35
Lukas Dembicki <lad_777@hotmail.com>
2024-02-09 12:16:49 +0000

    UPDATE: new route and CSS

--- src/components/ProfileContextMenu/index.js
+++ src/components/ProfileContextMenu/index.js
@@ -1,14 +1,18 @@
 import "./style.css";
 import ProfileIcon from "../../assets/icons/profileIcon";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
 
-const ProfileContextMenu = ({user}) => {
-  
-  const navigate = useNavigate()
-  
+const ProfileContextMenu = ({ user }) => {
+    
+    const navigate = useNavigate();
+
     return (
-        <div onClick={navigate(`/${user.id}`)} className="profile-context-menu">
-            <ProfileIcon/>
+        <div
+            onClick={navigate(`/users/${user.id}`)}
+            className="profile-context-menu"
+        >
+            <ProfileIcon />
             <div className="profile-text">Profile</div>
         </div>
     );
