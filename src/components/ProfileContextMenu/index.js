a3ec4dec6813018456ff264c70d2f46ca2ded56a
Lukas Dembicki <lad_777@hotmail.com>
2024-02-09 12:49:10 +0000

    FIX: CSS, position should stay with card now.

--- src/components/ProfileContextMenu/index.js
+++ src/components/ProfileContextMenu/index.js
@@ -3,16 +3,17 @@ import ProfileIcon from "../../assets/icons/profileIcon";
 import { useNavigate } from "react-router-dom";
 
 const ProfileContextMenu = ({ user }) => {
-    
     const navigate = useNavigate();
 
     return (
-        <div
-            onClick={navigate(`/users/${user.id}`)}
-            className="profile-context-menu"
-        >
-            <ProfileIcon />
-            <div className="profile-text">Profile</div>
+        <div className="menu-wrapper">
+            <div
+                onClick={navigate(`/users/${user.id}`)}
+                className="profile-context-menu"
+            >
+                <ProfileIcon />
+                <div className="profile-text">Profile</div>
+            </div>
         </div>
     );
 };
