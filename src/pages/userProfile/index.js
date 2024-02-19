761b276585108267ee629f266a51c59c797b4f42
Lukas Dembicki <lad_777@hotmail.com>
2024-02-19 11:32:11 +0000

    FEAT: added dummy data, some new placeholders

--- src/pages/userProfile/index.js
+++ src/pages/userProfile/index.js
@@ -2,14 +2,34 @@ import { useTranslation } from "react-i18next"
 import Form from "../../components/form"
 import TextInput from "../../components/form/textInput"
 import ProfileIcon from "../../assets/icons/profileIcon"
+import Card from "../../components/card"
+import ProfileCircle from "../../components/profileCircle"
+
+const user = {
+  firstName: "Lukas",
+  lastName: "Dembicki",
+  title: "Software Developer",
+  bio: "Hello I am a coder",
+  githubUsername: "PeachyOmnivore"
+}
+
+const initials = user && user.firstName && user.lastName ? `${user.firstName.charAt(0)}${user.lastName.charAt(0)}` : "A";
 
 const UserProfile = () => {
   const { t } = useTranslation()
+
+
   return (
     <main>
       <div className="welcome-formheader">
         <h3>{t("profile")}</h3>
       </div>
+      <Card>
+        <div className="">
+          <ProfileCircle initials={initials}/>
+          <h2>{user.firstName} {user.lastName}</h2>
+          <p>{user.title}</p>
+        </div>
       <div className="profile-container">
         <Form className="welcome-form">
           <div className="welcome-form-profileimg">
@@ -26,19 +46,19 @@ const UserProfile = () => {
             <TextInput
               name="firstName"
               label={`${t("firstName")} *`}
-              placeholder={`${t("enterYourFirstName")} *`}
+              placeholder={user.firstName ? user.firstName : `${t("enterYourFirstName")} *`}
               required
             />
             <TextInput
               name="lastName"
               label={`${t("lastName")} *`}
-              placeholder={`${t("enterYourLastName")} *`}
+              placeholder={user.lastName ? user.lastName : `${t("enterYourLastName")} *`}
               required
             />
             <TextInput
               name="githubUsername"
               label={`${t("githubUserName")} *`}
-              placeholder={`${t("enterYourGithubUser")} *`}
+              placeholder={user.githubUsername ? user.githubUsername : `${t("enterYourGithubUser")} *`}
               required
             />
             {/* {message && <p className="input-message">{message}</p>} */}
@@ -80,42 +100,8 @@ const UserProfile = () => {
             <p className="text-blue1">{`*${t("required")}`}</p>
           </div>
         </Form>
-
-        <Form className="welcome-form">
-            <div className="welcome-form-profileimg">
-              <p className="text-blue1">{t("photo")}</p>
-              <div className="welcome-form-profileimg-input">
-                <ProfileIcon colour="#28C846" background="#64DC78" />
-                <p className="text-blue1">{t("addHeadShot")}</p>
-              </div>
-              <p className="welcome-form-profileimg-error">
-                {t("uploadValidImage")}
-              </p>
-            </div>
-            <div className="welcome-form-inputs">
-              <TextInput
-                name="firstName"
-                label={`${t("firstName")} *`}
-                placeholder={`${t("enterYourFirstName")} *`}
-                required
-              />
-              <TextInput
-                name="lastName"
-                label={`${t("lastName")} *`}
-                placeholder={`${t("enterYourLastName")} *`}
-                required
-              />
-              <TextInput
-                name="githubUsername"
-                label={`${t("githubUserName")} *`}
-                placeholder={`${t("enterYourGithubUser")} *`}
-                required
-              />
-              {/* {message && <p className="input-message">{message}</p>} */}
-              <p className="text-blue1">{`*${t("required")}`}</p>
-            </div>
-          </Form>
       </div>
+      </Card>
     </main>
   )
 }
