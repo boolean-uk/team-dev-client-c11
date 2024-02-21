7b1fc89e37619aa42db673ecc9264484a9903e9e
Kye <NovusAa12@proton.me>
2024-02-21 15:22:29 +0000

    update myCohortDetails

--- src/components/myCohortDetails/index.js
+++ src/components/myCohortDetails/index.js
@@ -1,11 +1,12 @@
-import ProfileCircle from "../profileCircle";
-import UserCard from "../userCard";
-import Card from "../card";
-import "./style.css";
-import { useTranslation } from "react-i18next";
+import ProfileCircle from "../profileCircle"
+import UserCard from "../userCard"
+import Card from "../card"
+import "./style.css"
+import { useTranslation } from "react-i18next"
 
-const MyCohortDetails = ({ users }) => {
-  const {t} = useTranslation()
+const MyCohortDetails = ({ students, currentCohort }) => {
+  const { t } = useTranslation()
+
   return (
     <Card>
       <h3 className="my-cohort-details--header">{t("myCohort")}</h3>
@@ -13,18 +14,20 @@ const MyCohortDetails = ({ users }) => {
       <section className="my-cohort-details--cohort">
         <ProfileCircle initials="<>" />
         <div className="my-cohort-details--cohort-info">
-          <b>{t("softwareDevelopment")}, {t("cohort")} 1 </b>
+          <b>
+            {t("softwareDevelopment")}, {t("cohort")} {currentCohort}
+          </b>
           <p>August 2023 - March 2024</p>
         </div>
       </section>
 
       <ul className="my-cohort-details--list">
-        {users.map((user, idx) => (
+        {students.map((user, idx) => (
           <UserCard user={user} key={idx} />
         ))}
       </ul>
     </Card>
-  );
-};
+  )
+}
 
-export default MyCohortDetails;
+export default MyCohortDetails
