b7aa4ab0244ead7764b32ca7175dc4dbec6bb478
Kye <NovusAa12@proton.me>
2024-02-23 15:25:49 +0000

    update LoggedInUser component

--- src/components/loggedInUser/index.js
+++ src/components/loggedInUser/index.js
@@ -1,30 +1,29 @@
-import { useContext } from "react";
-import { AuthContext } from "../../context/auth";
-import ProfileCircle from "../profileCircle";
-
+import { useContext } from "react"
+import { AuthContext } from "../../context/auth"
+import ProfileCircle from "../profileCircle"
 
 const LoggedInUser = () => {
+  const loggedNow = useContext(AuthContext)
+  const { loggedInStudent, loggedInTeacher } = loggedNow
 
-    const loggedNow = useContext(AuthContext)
-    const { loggedInStudent, loggedInTeacher } = loggedNow
+  let initials = ""
 
-    let initials = ''
+  if (loggedInStudent) {
+    initials =
+      loggedInStudent?.firstName.charAt(0) + loggedInStudent?.lastName.charAt(0)
+  }
 
-    if (loggedInStudent) {
-        initials = loggedInStudent?.firstName.charAt(0) + loggedInStudent?.lastName.charAt(0)
-    }
+  if (loggedInTeacher) {
+    initials =
+      loggedInTeacher?.profile.firstName.charAt(0) +
+      loggedInTeacher?.profile.lastName.charAt(0)
+  }
 
-    if (loggedInTeacher) {
-        initials = loggedInTeacher?.user.profile.firstName.charAt(0) + loggedInTeacher?.user.profile.lastName.charAt(0)
-    }
-
-    return (
-        <>
-        <ProfileCircle initials={initials}/>
-        </>
-    )
+  return (
+    <>
+      <ProfileCircle initials={initials} />
+    </>
+  )
 }
 
-
 export default LoggedInUser
-
